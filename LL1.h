eccc2b1de3dee8a5ea9e7f62f885319bac4b7f03
LuStark <lance5158387@gmail.com>
2014-08-06 23:49:20 +0800

    nice shot

---
+++ LL1.h
@@ -0,0 +1,31 @@
+#ifndef _LL1_H_
+#define _LL1_H_
+
+#include "Tree.h"
+
+
+treenode Lisp();
+
+treenode exp();
+
+
+treenode Value();
+
+treenode procedure();
+
+treenode Operator();
+
+treenode LAMB();
+
+treenode exp_();
+
+treenode ArguRefList();
+
+
+treenode DEF();
+
+treenode DEFOBJ();
+
+treenode DEFBODY();
+
+#endif
