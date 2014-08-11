7fb12cb06892b5e29311cc20693bee3c93f626d1
LuStark <lance5158387@gmail.com>
2014-08-11 21:32:23 +0800

    define the tree class, and try to construct the AST in the LL1 program

--- Tree.h
+++ Tree.h
@@ -1,8 +1,39 @@
 #ifndef _TREE_H_
 #define _TREE_H_
 
-class treenode{
-    
+#include "Token.h"
+#include <vector>
+
+typedef class ASTNode *p_AstNode;
+
+class ASTNode{
+public:
+    ASTNode()=default;
+    ASTNode(const ASTNode& node){
+
+    }
+    void addChild( p_AstNode p )
+    {
+        child.push_back( p );
+    }
+    ASTNode( Token t ):token(t){}
+    friend std::ostream &operator<< ( std::ostream &out, p_AstNode pt)
+    {
+        out << pt->token;
+        return out;
+    }
+    Token &getToken()
+    {
+        return token;
+    }
+
+    void setChild(std::vector<p_AstNode> Q)
+    {
+        child = Q;
+    }
+private:
+    Token token;
+    std::vector<p_AstNode> child;
 };
 
 
