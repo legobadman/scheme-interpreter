e3aa5703620f7a5708402a5ec1cbf11079c6c96f
LuStark <lance5158387@gmail.com>
2014-08-15 18:59:08 +0800

    finish the operating overload for the Number Class, and divided the class definition into two file

--- Number.cpp
+++ Number.cpp
@@ -10,102 +10,84 @@ Number::Number( std::string str )
             this->type = FLOAT;
  
     if ( this->type==FLOAT )
-        val.f = atof(str.c_str());
+        f = atof(str.c_str());
     else
-        val.i = atoi(str.c_str());
+        this->i = atoi(str.c_str());
 
 }
 
 Number::Number( int i ) 
-    {
-        val.i = i;    
-        type = INT;
-    }
+{
+    i = i;    
+    type = INT;
+}
 
 Number::Number( float f )
-    {
-        val.f = f;
-        type = FLOAT;
-    }
+{
+    f = f;
+    type = FLOAT;
+}
 
 std::ostream &operator << ( std::ostream &out, Number num )
 {
     out << "NUM: ";
     if ( num.type == INT )
-        out << num.val.i;
+        out << num.i;
     else if( num.type == FLOAT )
-        out << num.val.f;
+        out << num.f;
     return out;
 }
 
 Number Number::operator + (const Number &numobj)
 {
-    if( this->type==FLOAT )
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.f + numobj.val.f) );
-        else
-            return Number( (float)(val.f + numobj.val.i) );
-    }
-    else
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.i + numobj.val.f) );
-        else
-            return Number( (int)(val.i + numobj.val.i) );
-    }
+    return Number( ((type==INT)? i : f) +
+                    ((numobj.type==INT)? numobj.i : numobj.f));
 }
 
 Number Number::operator - (const Number &numobj)
 {
-    if( this->type==FLOAT )
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.f - numobj.val.f) );
-        else
-            return Number( (float)(val.f - numobj.val.i) );
-    }
-    else
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.i - numobj.val.f) );
-        else
-            return Number( (int)(val.i - numobj.val.i) );
-    }
+    return Number( ((type==INT)? i : f) -
+                    ((numobj.type==INT)? numobj.i : numobj.f));
 }
 
 Number Number::operator * (const Number &numobj)
 {
-    if( this->type==FLOAT )
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.f * numobj.val.f) );
-        else
-            return Number( (float)(val.f * numobj.val.i) );
-    }
-    else
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.i * numobj.val.f) );
-        else
-            return Number( (int)(val.i * numobj.val.i) );
-    }
+    return Number( ((type==INT)? i : f) *
+                    ((numobj.type==INT)? numobj.i : numobj.f));
 }
 
 Number Number::operator / (const Number &numobj)
 {
-    if( this->type==FLOAT )
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.f / numobj.val.f) );
-        else
-            return Number( (float)(val.f / numobj.val.i) );
-    }
-    else
-    {
-        if( numobj.type == FLOAT )
-            return Number( (float)(val.i / numobj.val.f) );
-        else
-            return Number( (int)(val.i / numobj.val.i) );
-    }
+    return Number( ((type==INT)? i : f) /
+                    ((numobj.type==INT)? numobj.i : numobj.f));
 }
+
+bool Number::operator < (const Number &numobj)
+{
+    return (((type==INT)? i : f) < 
+                ((numobj.type==INT)?numobj.i : numobj.f));
+}
+
+bool Number::operator <= (const Number &numobj)
+{
+    return (((type==INT)? i : f) <=
+                ((numobj.type==INT)? numobj.i : numobj.f));
+}
+
+bool Number::operator > (const Number &numobj)
+{
+    return ((type==INT)?i:f ) > ((numobj.type==INT)? numobj.i : numobj.f);
+}
+
+bool Number::operator >= (const Number &numobj)
+{
+    return ((type==INT)? i : f) >= ((numobj.type==INT)? numobj.i : numobj.f);
+}
+
+bool Number::operator == (const Number &numobj)
+{
+    return ((type==INT)? i : f) == ((numobj.type==INT)? numobj.i : numobj.f);
+}
+
+
+
