4dc3326c1827a6285755c172d7ebd9549420ffe8
LuStark <lance5158387@gmail.com>
2014-08-15 18:59:08 +0800

    finish the operating overload for the Number Class, and divided the class definition into two file

--- Number.h
+++ Number.h
@@ -3,128 +3,42 @@
 
 #include <iostream>
 #include <cstdlib>
+#include "Fraction.h"
 
-enum DataType{ INT=1, FLOAT };
+/* support INT, FLOAT, FRACTION */
+enum DataType{ INT=1, FLOAT, FRACTION };
+
+//class Fraction;
 
 class Number
 {
 public:
     Number(){}
-    Number( std::string str )
-    {
-        int i;
-    
-        this->type = INT;
-        for( i=0; i<str.size(); i++ )
-            if ( str[i]=='.' )
-                this->type = FLOAT;
-    
-        if ( this->type==FLOAT )
-            val.f = atof(str.c_str());
-        else
-            val.i = atoi(str.c_str());
+    Number( std::string str );
+    Number( int i ); 
+    Number( float f );
 
-    }
-    Number( int i ) 
-    {
-        val.i = i;    
-        type = INT;
-    }
-    Number( float f )
-    {
-        val.f = f;
-        type = FLOAT;
-    }
-
-    int getInteger()const{ return val.i; }
-    float getFloat()const{ return val.f; }
+    int getInteger()const{ return i; }
+    float getFloat()const{ return f; }
     DataType getType()const{ return type; }
 
-    friend std::ostream &operator << ( std::ostream &out, Number num )
-    {
-        out << "NUM: ";
-        if ( num.type == INT )
-            out << num.val.i;
-        else if( num.type == FLOAT )
-            out << num.val.f;
-        return out;
-    }
-
+    friend std::ostream &operator << ( std::ostream &out, Number num );
     
-    Number operator + (const Number &numobj)
-    {
-        if( this->type==FLOAT )
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.f + numobj.val.f) );
-            else
-                return Number( (float)(val.f + numobj.val.i) );
-        }
-        else
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.i + numobj.val.f) );
-            else
-                return Number( (int)(val.i + numobj.val.i) );
-        }
-    }
-    Number operator - (const Number &numobj)
-    {
-        if( this->type==FLOAT )
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.f - numobj.val.f) );
-            else
-                return Number( (float)(val.f - numobj.val.i) );
-        }
-        else
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.i - numobj.val.f) );
-            else
-                return Number( (int)(val.i - numobj.val.i) );
-        }
-    }
-    Number operator * (const Number &numobj)
-    {
-        if( this->type==FLOAT )
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.f * numobj.val.f) );
-            else
-                return Number( (float)(val.f * numobj.val.i) );
-        }
-        else
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.i * numobj.val.f) );
-            else
-                return Number( (int)(val.i * numobj.val.i) );
-        }
-    }
-    Number operator / (const Number &numobj)
-    {
-        if( this->type==FLOAT )
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.f / numobj.val.f) );
-            else
-                return Number( (float)(val.f / numobj.val.i) );
-        }
-        else
-        {
-            if( numobj.type == FLOAT )
-                return Number( (float)(val.i / numobj.val.f) );
-            else
-                return Number( (int)(val.i / numobj.val.i) );
-        }
-    }
+    Number operator + (const Number& );
+    Number operator - (const Number& );
+    Number operator * (const Number& );
+    Number operator / (const Number& );
+    
+    bool operator < (const Number& );
+    bool operator <= (const Number& );
+    bool operator > (const Number& );
+    bool operator >= (const Number& );
+    bool operator == (const Number& );
 
 private:
-    union{
-        float   f;
-        int     i;
-    }val;
+    float   f;
+    int     i;
+    Fraction ff; 
     DataType type;
 };
 
