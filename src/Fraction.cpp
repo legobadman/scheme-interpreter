da3bbfef0972e3a43afcbba45d0c7330253259ca
LuStark <lance5158387@gmail.com>
2014-08-16 09:51:23 +0800

    fraction in lowest term

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -1,5 +1,47 @@
 #include "Fraction.h"
 
+#define abs(a) ((a)>=0)?(a):-a
+
+static int gcd(int a,int b)
+{
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+
+
+Fraction::Fraction(const Fraction& f)
+{
+    int n = abs(f.numer);
+    int d = abs(f.denom);
+
+    int gcd_num;
+
+    if( n>d )
+        gcd_num = gcd(n,d);
+    else
+        gcd_num = gcd(d,n);
+
+    numer = f.numer / gcd_num;
+    denom = f.denom / gcd_num;
+}
+
+Fraction::Fraction(int n, int d)
+{
+    int absn = abs(n);
+    int absd = abs(d);
+
+    int gcd_num;
+
+    if( absn>absd )
+        gcd_num = gcd(absn,absd);
+    else
+        gcd_num = gcd(absd,absn);
+
+    numer = n / gcd_num;
+    denom = d / gcd_num;
+}
+
 Fraction Fraction::operator + (const Fraction &f) const
 {
     return Fraction( numer*f.denom + f.numer*denom, denom*f.denom );
@@ -43,7 +85,10 @@ Fraction Fraction::operator / (int a) const
 
 ostream &operator << ( ostream &out, Fraction &f )
 {
-    out << f.numer << "/" << f.denom;
+    if( f.numer % f.denom == 0)
+        out << f.numer / f.denom;
+    else
+        out << f.numer << "/" << f.denom;
     return out;
 }
 
