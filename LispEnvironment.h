2d30e8be2243e0ee0ac4f53cfd08df57ae320acb
LuStark <lance5158387@gmail.com>
2014-08-17 21:43:47 +0800

    define the singleton class LispEnvironment, for wrapping some info like symbol table,etc

---
+++ LispEnvironment.h
@@ -0,0 +1,40 @@
+#ifndef _LISP_ENV_H_
+#define _LISP_ENV_H_
+
+#include <cstdlib>
+#include <map>
+#include "Tree.h"
+
+class LispEnvironment
+{
+public:
+    static LispEnvironment &getRunTimeEnv()
+    {
+        if( prisoner == NULL )
+            prisoner = new LispEnvironment();
+        return *prisoner;
+    }
+private:
+    static LispEnvironment *prisoner;
+    LispEnvironment(){}
+
+    /* in order to distinguish whether to execute the 
+     * calculation without generating a parsed Tree.
+     * 1 means that we can work out such as (+ a b) 
+     * directly.
+     
+     * otherwise, for example, defining a
+     * function like (f x y) (+ x y), can not calculate
+     * because it's not a expression for calculating, we
+     * will generate a parsed tree as a function model
+     */
+    int is_directly_calculating; 
+
+    /* symbol table for the whole runtime */
+    map<string, p_AstNode> SymbolTable;
+};
+
+
+
+
+#endif
