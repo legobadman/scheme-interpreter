4e33f651f7c919b9676df4d7f75f11467b79fff4
LuStark <lance5158387@gmail.com>
2014-08-17 11:16:42 +0800

    test module

--- test/test_fraction.cpp
+++ test/test_fraction.cpp
@@ -8,8 +8,18 @@ int main()
     int x = 3;
     Fraction f1(3);
     Fraction f2(2,3);
-    cout << f1 << endl;
-    cout << f2 << endl;
+    Fraction f3;
 
-    //cout << f1*f2 << endl;
+    f3 = f1+f2;
+    cout << f1 << " + " << f2 << " = " << f3 << endl;
+    
+    f3 = f1*f2;
+    cout << f1 << " * " << f2 << " = " << f3 << endl;
+
+    Fraction f4 = f1/f2;
+    cout << f1 << " / " << f2 << " = " << f4 << endl;
+
+    f4 = 3 + f3;
+    cout << f3 << " / " << 3 << " = " << f4 << endl;
+
 }
