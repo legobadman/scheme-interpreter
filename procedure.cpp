68e2f700fbbae3d12a97579bdbf41ce597366dcc
LuStark <lance5158387@gmail.com>
2014-08-17 16:35:40 +0800

    build-in method, and gradually abandon the instant calculation

---
+++ procedure.cpp
@@ -0,0 +1,30 @@
+#include "Tree.h"
+#include "LL1.h"
+
+#include <iostream>
+using namespace std;
+
+
+p_AstNode add( vector<p_AstNode> &dataSeq )
+{
+
+}
+
+p_AstNode sub( vector<p_AstNode> &dataSeq )
+{
+
+
+}
+
+p_AstNode multiply( vector<p_AstNode> &dataSeq )
+{
+    
+}
+
+p_AstNode divide( vector<p_AstNode> &dataSeq )
+{
+
+
+}
+
+
