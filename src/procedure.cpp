e0b8409884251e76f78b8fb83c03d0c7a4589ffe
LuStark <lance5158387@gmail.com>
2014-08-18 01:40:30 +0800

    血淋淋的重构

--- src/procedure.cpp
+++ src/procedure.cpp
@@ -1,5 +1,6 @@
 #include "Tree.h"
 #include "LL1.h"
+#include "LispEnvironment.h"
 
 #include <iostream>
 using namespace std;
@@ -7,24 +8,57 @@ using namespace std;
 
 p_AstNode add( vector<p_AstNode> &dataSeq )
 {
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+    
 
 }
 
 p_AstNode sub( vector<p_AstNode> &dataSeq )
 {
 
-
 }
 
 p_AstNode multiply( vector<p_AstNode> &dataSeq )
 {
-    
+
 }
 
 p_AstNode divide( vector<p_AstNode> &dataSeq )
 {
 
+}
+
+p_AstNode greater_than( vector<p_AstNode>& )
+{
+
 
 }
 
+p_AstNode greater_or_equal_than( vector<p_AstNode>& )
+{
+
+}
+
+p_AstNode less_than( vector<p_AstNode>& )
+{
+
+}
+
+p_AstNode less_or_equal_than( vector<p_AstNode>& )
+{
+
+}
+
+p_AstNode is_equal( vector<p_AstNode>& )
+{
+
+
+}
+
+p_AstNode define( p_AstNode defObj, p_AstNode defBody )
+{
+
+
+
+}
 
