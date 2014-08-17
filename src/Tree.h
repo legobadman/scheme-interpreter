b2567e029651630de2ce279b5b2ba3f9d8bbd3b3
LuStark <lance5158387@gmail.com>
2014-08-17 11:15:41 +0800

    define enum type TreeNode

--- src/Tree.h
+++ src/Tree.h
@@ -8,10 +8,11 @@ typedef class ASTNode *p_AstNode;
 
 extern p_AstNode CalculateAST( p_AstNode ast_root );
 
+enum NodeType{ BOOLEAN_NODE, NUM_NODE, PROC_NODE, CONS_NODE, LIST_NODE };
 
 class ASTNode{
 public:
-    ASTNode()=default;
+    ASTNode(){}
     ASTNode(const ASTNode& node){
 
     }
@@ -27,15 +28,28 @@ public:
     {
         child.push_back( p );
     }
+
     friend std::ostream &operator<< ( std::ostream &out, p_AstNode pt)
     {
-        out << pt->token;
+        switch(pt->type)
+        {
+        case CONS_NODE: 
+            out << "(" << pt->child[0]->getToken().getNumber()
+                << " . " << pt->child[1]->getToken().getNumber()
+                << ")";
+            break;
+        default:
+            out << pt->getToken().getNumber();
+            break;
+        }
         return out;
     }
+
     Token &getToken()
     {
         return token;
     }
+
     std::vector<p_AstNode> &getChild()
     {
         return child;
@@ -46,8 +60,16 @@ public:
         child = Q;
     }
 
+    void setNodeType(NodeType t)
+    {
+        type = t;
+    }
+
 private:
     Token token;
+
+    NodeType type;
+
     std::vector<p_AstNode> child;
 };
 
