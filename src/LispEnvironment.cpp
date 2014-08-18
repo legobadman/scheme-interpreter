3f8a5cbd8f84e0950be8c8fa914ae5102a626dd3
LuStark <lance5158387@gmail.com>
2014-08-18 23:39:53 +0800

    adjust the relation between env and symbol table

--- src/LispEnvironment.cpp
+++ src/LispEnvironment.cpp
@@ -28,3 +28,33 @@ p_AstNode LispEnvironment::ReadOut(p_AstNode p)
 
     return finalNode;
 }
+
+
+void LispEnvironment::pushArgumentInStack(vector<p_AstNode> &arguList)const
+{
+    vector<p_AstNode>::iterator it;
+ 
+    vector<SymbolTable>::reverse_iterator T = prisoner->runTimeStack.rbegin();
+
+    for( it = arguList.begin(); it != arguList.end(); it++ )
+    {
+        (*T).insertID( (*it)->getName(), (*it) );
+    }
+}
+
+
+p_AstNode LispEnvironment::getSymbol( string idName )
+{
+    vector<SymbolTable>::reverse_iterator it = prisoner->runTimeStack.rbegin();
+
+    return (*it).getSymbol( idName );
+}
+
+void LispEnvironment::InsertID( string idName, p_AstNode defNode )
+{
+    vector<SymbolTable>::reverse_iterator it;
+    it = prisoner->runTimeStack.rbegin();
+        
+    (*it).insertID( idName, defNode );
+}
+
