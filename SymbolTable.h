a769ca8c747c1c61bae1d87c754b4d6eb1c385e7
LuStark <lance5158387@gmail.com>
2014-08-18 23:25:28 +0800

    implement symbol table

--- SymbolTable.h
+++ SymbolTable.h
@@ -1,9 +1,22 @@
 #ifndef _SYMBOL_TABLE_
 #define _SYMBOL_TABLE_
 
+#include <map>
+#include "Tree.h"
+#include "tokenType.h"
+#include <iostream>
+
 class SymbolTable
 {
+public:
+    /* insert the node, coresponding to the name */
+    void insertID( string idName, p_AstNode node );
 
+
+    p_AstNode getSymbol( string idName );
+
+private:
+    map<std::string,p_AstNode> T;
 };
 
 
