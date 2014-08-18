0dcf9dc123684807c03a31ee6b0d188aa812bc60
LuStark <lance5158387@gmail.com>
2014-08-18 14:34:21 +0800

    can define a identifier

--- LispEnvironment.cpp
+++ LispEnvironment.cpp
@@ -2,3 +2,29 @@
 
 
 LispEnvironment* LispEnvironment::prisoner = NULL;
+
+
+p_AstNode LispEnvironment::ReadOut(p_AstNode p)
+{
+    p_AstNode idNode, bodyNode, finalNode;
+
+    switch( p->getTokenType() )
+    {
+    case DEFINE:
+        idNode = p->getOneChild(0);
+        bodyNode = p->getOneChild(1);
+        if( idNode->getChild().size() == 0 )
+        {
+            // 单个标识符
+            finalNode = bodyNode;
+        }
+        else
+        {
+            // 函数
+        }
+    default:
+        break;
+    }
+
+    return finalNode;
+}
