40b16c2d36043e5ce06f4fbb3854d0197c7b3517
LuStark <lance5158387@gmail.com>
2014-08-18 23:25:28 +0800

    implement symbol table

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,1 +1,25 @@
 #include "SymbolTable.h"
+
+void SymbolTable::insertID( string idName, p_AstNode node )
+{
+    //对于已有的建，尝试覆盖
+    T.insert( pair<std::string,p_AstNode>( idName, node ));
+}
+
+
+
+p_AstNode SymbolTable::getSymbol( string idName )
+{
+ 
+    map<std::string, p_AstNode>::iterator it;
+    it = T.find( idName );
+
+    if( it != T.end() )
+    {
+        return it->second;
+    }
+    else
+    {
+        return NULL;
+    }
+}
