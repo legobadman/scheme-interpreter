8b0f6dabecd0f5d393b90b5694c70fa66cf365ba
LuStark <lance5158387@gmail.com>
2014-08-19 17:07:39 +0800

    interpreter implements

--- src/procedure.cpp
+++ src/procedure.cpp
@@ -1,66 +1,165 @@
 #include "Tree.h"
 #include "LL1.h"
 #include "LispEnvironment.h"
+#include "procedure.h"
+#include <cstdlib>
 
 #include <iostream>
 using namespace std;
 
 
-p_AstNode add( vector<p_AstNode> &dataSeq )
+p_AstNode add( vector<p_AstNode> &ValueList )
 {
     LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    p_AstNode   element;
+    Number  sum(0);
     
+    it = ValueList.begin();
+    for( ; it != ValueList.end(); it++ )
+    {
+        element = (*it);
+        sum = sum + interpreter( element )->getNumber();
+    }
+    return new ASTNode( sum );
+}
+
+p_AstNode sub( vector<p_AstNode> &ValueList )
+{
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
     
+    p_AstNode   element;
+    Number  result;
+    
+    it = ValueList.begin();
+    result = (*it)->getNumber();
 
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        element = (*it);
+        result = result - interpreter( element )->getNumber();
+    }
+
+    return new ASTNode( result );
 
 }
 
-p_AstNode sub( vector<p_AstNode> &dataSeq )
+p_AstNode multiply( vector<p_AstNode> &ValueList )
 {
-
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    p_AstNode   element;
+    Number  product(1);
+    
+    it = ValueList.begin();
+    for( ; it != ValueList.end(); it++ )
+    {
+        element = (*it);
+        product = product * interpreter( element )->getNumber();
+    }
+    return new ASTNode( product );
 }
 
-p_AstNode multiply( vector<p_AstNode> &dataSeq )
+
+p_AstNode divide( vector<p_AstNode> &ValueList )
 {
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    
+    p_AstNode   element;
+    Number  result;
+    
+    it = ValueList.begin();
+    result = (*it)->getNumber();
+
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        element = (*it);
+        result = result / interpreter( element )->getNumber();
+    }
+
+    return new ASTNode( result );
 
 }
 
-p_AstNode divide( vector<p_AstNode> &dataSeq )
+p_AstNode interpreter( p_AstNode root )
 {
+    string rootName = root->getName();
+    vector<p_AstNode> &childList = root->getChild();
+    
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
 
+    p_AstNode   result;
+
+    switch( root->getTokenType() )
+    {
+    case ID:
+        result = env.getSymbol( rootName );
+        if( !result )
+        {
+            cerr << "undefined symbol: " << rootName << endl;
+            exit(0);
+        }
+        //delete root;
+        break;
+
+    case NUM:
+        result = root;
+        break;
+
+    case ARITH_OP:
+        if( rootName == "+" )
+        {
+            result = add( childList );
+        }
+        else if( rootName == "-" )
+        {
+            result = sub( childList );
+        }
+        else if( rootName == "*" )
+        {
+            result = multiply( childList );
+        }
+        else if( rootName == "/" )
+        {
+            result = divide( childList );
+        }
+
+        for(int i=0; i<childList.size(); i++ )
+            delete childList[i];
+        childList.clear();
+
+        break;
+    case BOOL_OP:
+        if( rootName == "<" )
+        {
+
+        }
+        else if( rootName == "<=" )
+        {
+
+        }
+        else if( rootName == ">" )
+        {
+
+        }
+        else if( rootName == ">=" )
+        {
+
+        }
+        else if( rootName == "=" )
+        {
+            
+        }
+        break;
+    case PROC:
+        break;
+    }
+
+    return result;
 }
-
-p_AstNode greater_than( vector<p_AstNode>& )
-{
-
-
-}
-
-p_AstNode greater_or_equal_than( vector<p_AstNode>& )
-{
-
-}
-
-p_AstNode less_than( vector<p_AstNode>& )
-{
-
-}
-
-p_AstNode less_or_equal_than( vector<p_AstNode>& )
-{
-
-}
-
-p_AstNode is_equal( vector<p_AstNode>& )
-{
-
-
-}
-
-p_AstNode define( p_AstNode defObj, p_AstNode defBody )
-{
-
-
-
-}
-
