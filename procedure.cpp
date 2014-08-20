399cf38e7ab0e81ceefd64e70b4aebd25f6a67ce
LuStark <lance5158387@gmail.com>
2014-08-20 09:09:35 +0800

    方法定义初次运作

--- procedure.cpp
+++ procedure.cpp
@@ -135,31 +135,286 @@ p_AstNode interpreter( p_AstNode root )
         childList.clear();
 
         break;
+
     case BOOL_OP:
         if( rootName == "<" )
         {
-
+            result = less_than( childList );
         }
         else if( rootName == "<=" )
         {
-
+            result = less_or_equal_than( childList );
         }
         else if( rootName == ">" )
         {
-
+            result = greater_than( childList );
         }
         else if( rootName == ">=" )
         {
-
+            result = greater_or_equal_than( childList );
         }
         else if( rootName == "=" )
         {
-            
+            result = is_equal( childList );
         }
+
         break;
+    
     case PROC:
+        result = callProcedure( rootName, childList );
+        result = interpreter( result );
         break;
+
+    case ARGUMENT:
+
+
+    case IF:
+        /* reguard any positive as true symbol */
+        if( interpreter( root->getOneChild(0) ) > 0 )
+        {
+            result = interpreter(root->getOneChild(1)); 
+        }
+        else
+        {
+            result = interpreter(root->getOneChild(2));
+        }
     }
 
     return result;
 }
+
+
+
+
+p_AstNode greater_than( vector<p_AstNode> &ValueList )
+{
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    
+    p_AstNode   last_element, current_element;
+    Number  lastNum, currentNum;
+    
+    it = ValueList.begin();
+    lastNum = interpreter( (*it) )->getNumber();
+
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        current_element = (*it);
+        currentNum = interpreter( current_element )->getNumber();
+        if( lastNum <= currentNum )
+        {
+            return new ASTNode( Number(0) );
+        }
+
+        lastNum = currentNum;
+    }
+
+    return new ASTNode( Number(1) );
+
+}
+
+
+p_AstNode greater_or_equal_than( vector<p_AstNode> &ValueList )
+{
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    
+    p_AstNode   last_element, current_element;
+    Number  lastNum, currentNum;
+    
+    it = ValueList.begin();
+
+    lastNum = interpreter( (*it) )->getNumber();
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        current_element = (*it);
+        currentNum = interpreter( current_element )->getNumber();
+        if( lastNum < currentNum )
+            return new ASTNode( Number(0) );
+
+        lastNum = currentNum;
+    }
+
+    return new ASTNode( Number(1) );
+
+}
+
+
+p_AstNode less_than( vector<p_AstNode>& ValueList)
+{
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    
+    p_AstNode   last_element, current_element;
+    Number  lastNum, currentNum;
+    
+    it = ValueList.begin();
+
+    lastNum = interpreter( (*it) )->getNumber();
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        current_element = (*it);
+        currentNum = interpreter( current_element )->getNumber();
+        if( lastNum >= currentNum )
+        {
+            return new ASTNode( Number(0) );
+        }
+
+        lastNum = currentNum;
+    }
+
+    return new ASTNode( Number(1) );
+
+}
+
+p_AstNode less_or_equal_than( vector<p_AstNode> &ValueList )
+{
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    
+    p_AstNode   last_element, current_element;
+    Number  lastNum, currentNum;
+    
+    it = ValueList.begin();
+    lastNum = interpreter( (*it) )->getNumber();
+
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        current_element = (*it);
+        currentNum = interpreter( current_element )->getNumber();
+        if( lastNum > currentNum )
+        {
+            return new ASTNode( Number(0) );
+        }
+
+        lastNum = currentNum;
+    }
+
+    return new ASTNode( Number(1) );
+
+}
+
+p_AstNode is_equal( vector<p_AstNode> &ValueList )
+{
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+        
+    vector<p_AstNode>::iterator it;
+    
+    p_AstNode   last_element, current_element;
+    Number  lastNum, currentNum;
+    
+    it = ValueList.begin();
+    lastNum = interpreter( (*it) )->getNumber();
+
+    for( ++it; it != ValueList.end(); it++ )
+    {
+        current_element = (*it);
+        currentNum = interpreter( current_element )->getNumber();
+        if( currentNum != lastNum )
+        {
+            return new ASTNode( Number(0) );
+        }
+
+        lastNum = currentNum;
+    }
+
+    return new ASTNode( Number(1) );
+
+}
+
+p_AstNode And( vector<p_AstNode> &ValueList )
+{
+}
+
+p_AstNode Or( vector<p_AstNode> &ValueList )
+{
+}
+
+p_AstNode Not( vector<p_AstNode> &ValueList )
+{
+
+}
+
+
+vector<p_AstNode> getFormalArgument( const p_AstNode procNode )
+{
+    return procNode->getOneChild(0)->getChild();
+}
+
+p_AstNode callProcedure( string procName, vector<p_AstNode> ValueList )
+{
+    p_AstNode procNode;
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+
+    procNode = env.getSymbol( procName );
+
+    if( !procNode )
+    {
+        cerr << "Undefined procedure: "<< procName << endl;
+        exit(0);
+    }
+
+    vector<p_AstNode> formalArgument = getFormalArgument( procNode );
+    if( ValueList.size() != formalArgument.size() )
+    {
+        cerr << "The arguments didn't match" << endl;
+    }
+
+    map<string,Number> actualArgument;
+
+    for( int i=0; i<formalArgument.size(); i++ )
+    {
+        string arguName = formalArgument[i]->getName();
+        Number arguValue = interpreter( ValueList[i] )->getNumber();
+
+        actualArgument[ arguName ] = arguValue;
+    }
+
+    procNode = procNode -> getOneChild(1);
+    p_AstNode assignedTree = assignArgument( procNode, actualArgument );
+
+    return assignedTree;
+}
+
+
+p_AstNode assignArgument( const p_AstNode root, map<string,Number> actualArgument )
+{
+    
+    vector<p_AstNode> childList = root->getChild();
+    LispEnvironment env = LispEnvironment::getRunTimeEnv();
+    TokenType type = root->getTokenType();
+    string name = root->getName();
+    
+    p_AstNode newNode;
+
+    if( type == ARGUMENT )
+    {
+        newNode = new ASTNode( actualArgument[root->getName()] );
+    }
+    else if( type == NUM )
+    {
+        Number numobj = root->getNumber();
+        newNode = new ASTNode( numobj );
+    }
+    else if( type == ID )
+    {
+        newNode = new ASTNode( env.getSymbol( name )->getNumber() );
+    }
+    else
+    {
+        newNode = new ASTNode( type, name ); 
+        for( int i=0; i<childList.size(); i++ )
+        {
+            p_AstNode newChild = assignArgument( childList[i], actualArgument );
+            newNode->addChild( newChild );
+        }
+    }
+
+    return newNode;
+}
+
+
