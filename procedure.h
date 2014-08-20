0d7434ce3188756dff2a415daee20ee1415dcb71
LuStark <lance5158387@gmail.com>
2014-08-20 09:09:35 +0800

    方法定义初次运作

--- procedure.h
+++ procedure.h
@@ -2,7 +2,7 @@
 #define _PROCEDURE_H_
 
 #include "Tree.h"
-
+#include <map>
 
 p_AstNode interpreter( p_AstNode );
 
@@ -25,6 +25,21 @@ p_AstNode less_than( vector<p_AstNode>& );
 p_AstNode less_or_equal_than( vector<p_AstNode>& );
 
 p_AstNode is_equal( vector<p_AstNode>& ); 
+
+p_AstNode And( vector<p_AstNode>& );
+
+p_AstNode Or( vector<p_AstNode>& );
+
+p_AstNode Not( vector<p_AstNode>& );
+
+
+vector<p_AstNode> getFormalArgument( const p_AstNode procNode );
+
+p_AstNode callProcedure( string procName, vector<p_AstNode> ValueList );
+
+p_AstNode assignArgument( const p_AstNode root, map<string,Number> actualArgument );
+
+
 
 
 #endif
