e019dd2865e8f6996b04e07c3ac5d65fd6aea8e2
LuStark <lance5158387@gmail.com>
2014-08-22 19:29:47 +0800

    new folder

---
+++ src/procedure.h
@@ -0,0 +1,48 @@
+#ifndef _PROCEDURE_H_
+#define _PROCEDURE_H_
+
+#include "Tree.h"
+#include <map>
+
+p_AstNode interpreter( p_AstNode );
+
+
+p_AstNode substitudeArgument( p_AstNode lambNode, vector<p_AstNode> valueList );
+
+p_AstNode add( vector<p_AstNode> &);
+
+p_AstNode sub( vector<p_AstNode> &);
+
+p_AstNode multiply( vector<p_AstNode> &);
+
+p_AstNode divide( vector<p_AstNode> &);
+
+p_AstNode greater_than( vector<p_AstNode>& );
+
+p_AstNode greater_or_equal_than( vector<p_AstNode>& );
+
+p_AstNode less_than( vector<p_AstNode>& );
+
+p_AstNode less_or_equal_than( vector<p_AstNode>& );
+
+p_AstNode is_equal( vector<p_AstNode>& ); 
+
+p_AstNode And( vector<p_AstNode>& );
+
+p_AstNode Or( vector<p_AstNode>& );
+
+p_AstNode Not( vector<p_AstNode>& );
+
+
+vector<p_AstNode> getFormalArgument( const p_AstNode procNode );
+
+p_AstNode callProcedure (string procName, vector<p_AstNode> ValueList );
+
+p_AstNode callTemperoryProcedure (p_AstNode);
+
+p_AstNode assignArgument (const p_AstNode, map<string, p_AstNode>);
+
+
+
+
+#endif
