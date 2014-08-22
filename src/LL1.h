e436ee81011b343ada4e2bce6ce9d076b0654d68
LuStark <lance5158387@gmail.com>
2014-08-22 19:29:47 +0800

    new folder

---
+++ src/LL1.h
@@ -0,0 +1,49 @@
+#ifndef _LL1_H_
+#define _LL1_H_
+
+#include "Tree.h"
+
+
+
+
+p_AstNode LL1_Lisp();
+
+p_AstNode LL1_exp();
+
+
+p_AstNode LL1_Value();
+
+p_AstNode LL1_procedure();
+
+p_AstNode LL1_Operator();
+
+p_AstNode LL1_Rop();
+p_AstNode LL1_Boolop();
+
+p_AstNode LL1_IF();
+p_AstNode LL1_COND();
+p_AstNode LL1_ConditionList();
+
+p_AstNode LL1_LET();
+
+p_AstNode LL1_LAMB();
+
+std::vector<p_AstNode> LL1_exp_();
+
+std::vector<p_AstNode> LL1_ArguRefList();
+
+
+p_AstNode LL1_DEF();
+
+p_AstNode LL1_DEFOBJ();
+
+p_AstNode LL1_DEFBODY();
+
+
+p_AstNode LL1_CONS();
+p_AstNode LL1_CAR();
+p_AstNode LL1_CDR();
+
+p_AstNode LL1_LIST();
+
+#endif
