5712a6c7ed22ad56ae6c4278a33f36e93e85cd86
LuStark <lance5158387@gmail.com>
2014-08-22 19:29:47 +0800

    new folder

---
+++ src/SymbolTable.h
@@ -0,0 +1,27 @@
+#ifndef _SYMBOL_TABLE_
+#define _SYMBOL_TABLE_
+
+#include <map>
+#include "Tree.h"
+#include "tokenType.h"
+#include <iostream>
+
+class SymbolTable
+{
+public:
+    /* insert the node, coresponding to the name */
+    void insertID( string idName, p_AstNode node );
+    void deleteID( string idName );
+    
+    ~SymbolTable();
+
+    p_AstNode getSymbol( string idName );
+    
+    void outputTable();
+
+private:
+    map<std::string,p_AstNode> T;
+};
+
+
+#endif
