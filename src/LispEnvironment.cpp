b8d753fcf161a39c63e6ada50090d2e15bd42b01
LuStark <lance5158387@gmail.com>
2014-08-22 19:29:47 +0800

    new folder

---
+++ src/LispEnvironment.cpp
@@ -0,0 +1,81 @@
+#include "LispEnvironment.h"
+#include "procedure.h"
+
+
+LispEnvironment* LispEnvironment::prisoner = NULL;
+
+
+
+void LispEnvironment::pushArgumentInStack(vector<p_AstNode> &arguList)const
+{
+    vector<p_AstNode>::iterator it;
+ 
+    vector<SymbolTable>::reverse_iterator T = prisoner->runTimeStack.rbegin();
+
+    for( it = arguList.begin(); it != arguList.end(); it++ )
+    {
+        (*T).insertID( (*it)->getName(), (*it) );
+    }
+}
+
+
+p_AstNode LispEnvironment::getSymbol( string idName )
+{
+    vector<SymbolTable>::reverse_iterator it = prisoner->runTimeStack.rbegin();
+    
+    p_AstNode p = NULL;
+    for( ; it != prisoner->runTimeStack.rend(); it++ )
+    {
+        p = (*it).getSymbol( idName );
+        if( p!=NULL )
+            break;
+    }
+
+    return p;
+}
+
+bool LispEnvironment::isSymbolInCurrentStack( string idName )
+{
+    vector<SymbolTable>::reverse_iterator it = prisoner->runTimeStack.rbegin();
+    return (*it).getSymbol( idName );
+}
+
+void LispEnvironment::InsertID( string idName, p_AstNode defNode )
+{
+    vector<SymbolTable>::reverse_iterator it;
+    it = prisoner->runTimeStack.rbegin();
+        
+    (*it).insertID( idName, defNode );
+}
+
+
+void LispEnvironment::runTimeStackPush()
+{
+    prisoner -> runTimeStack.push_back( SymbolTable() );
+}
+
+void LispEnvironment::runTimeStackPop()
+{
+    prisoner -> runTimeStack.pop_back();
+}
+
+
+void LispEnvironment::outputCurrentSymbolTable()
+{
+    vector<SymbolTable>::reverse_iterator it;
+    it = prisoner->runTimeStack.rbegin();
+
+    (*it).outputTable();
+
+}
+
+void LispEnvironment::DeleteID( string idName )
+{
+    vector<SymbolTable>::reverse_iterator it;
+    it = prisoner->runTimeStack.rbegin();
+    
+    (*it).deleteID( idName );
+}
+
+
+
